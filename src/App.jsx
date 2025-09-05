e00934e443fe7b07615a3db8e56f6776a61ab3d3
Jasmine <oatman.estates.holdings@gmail.com>
2025-09-05 13:22:45 -0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import Nav from './components/Nav.jsx'
+import Nav from './components/Nav.jsx';
 
 
 function App() {
