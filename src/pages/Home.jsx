41b7480184f945f2622eb07a41ff205484cbb0f3
Jasmine <oatman.estates.holdings@gmail.com>
2025-09-08 13:18:51 -0400

    dynamic react api integration

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import axios from 'axios'
+import User from "../components/Users.jsx";
+
 function Home() {
-    return <h1>Home</h1>
+    const [users, setUsers] = useState([]);
+
+    async function fetchUsers() {
+        const { data } = await axios.get(
+            "https://jsonplaceholder.typicode.com/users"
+        );
+        setUsers(data);
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+        fetchUsers();
+    }, 500);
+ }, []);
+
+
+    return (
+    <div>
+        {users.map((user) => (
+        <User
+        key={user.id} 
+        id={user.id} 
+        name={user.name} 
+        email={user.email} 
+        username={user.username}
+        />
+        ))}
+    </div>
+    );
 }
 
-export default Home
+export default Home;
